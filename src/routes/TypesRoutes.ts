91604464dd6ada772fdc960206491b6606ead0ba
Julio Rodríguez <julio10489@gmail.com>
2024-03-01 17:07:52 -0500

    Added: Primeras rutas e instalacion de react-router

---
+++ src/routes/TypesRoutes.ts
@@ -0,0 +1,25 @@
+
+export const PUBLIC_ROUTES = {
+  HOME: "/",
+  SEARCH: "/:product-name",
+  PRODUCTS: "/products",
+  VIEW_PRODUCT: "/products/:id",
+  ABOUT: "/about-us",
+  LOGIN: "/login",
+  SINGIN: "/signin",
+  404: "/*"
+}
+export const PRIVATE_USER_ROUTES = {
+  PROFILE_CONFIG: "/profile",
+  CART: "/cart",
+  MAKE_BUY: "/confirm_buy",
+  SHOPPING_HISTORY: "/shopping-history",
+}
+export const PRIVATE_SELLER_ROUTES = {
+  REPORTS_AREA: "/reports-area/:idArea",
+  EDIT_PRODUCTS: "/products/edit",
+  CREATE_REPORTS: "/reports-area/:idArea/create"
+}
+export const PRIVATE_ADMIN_ROUTES = {
+  
+}
