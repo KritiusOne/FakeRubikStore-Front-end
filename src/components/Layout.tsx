c4932593eb629c47749a30f2a54928c818638cbf
Julio Rodríguez <julio10489@gmail.com>
2024-03-06 11:45:34 -0500

    Created: First Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,5 @@
 import { HTMLAttributes } from "react"
+import { AsideMenu } from "./AsideMenu"
 import { NavegationMenuHeader } from "./NavegationMenuHeader"
 
 interface Props extends HTMLAttributes<HTMLDivElement> {
@@ -6,11 +7,20 @@ interface Props extends HTMLAttributes<HTMLDivElement> {
 }
 export const Layout: React.FC<Props> = ({children, ...otherProps})=>{
   return (
-    <div className="min-h-screen min-w-screen bg-slate-950 text-slate-50 flex flex-col mx-auto items-center" {...otherProps}>
-      <NavegationMenuHeader url="LOGO" />
-      {
-        children
-      }
+    <div className="bg-bgDark text-white min-w-screen min-h-screen
+    grid grid-cols-4 grid-rows-3" {...otherProps}>
+      <AsideMenu className="w-full col-span-1 row-span-full bg-bgLight"  />
+      <main className="col-start-2 col-end-5 row-start-1 row-end-4 bg-tomato flex flex-col justify-center items-center grid-start-2 grid-end-3">
+        <NavegationMenuHeader className="" />
+        <section>
+          {
+            children
+          }
+        </section>
+      </main>
+      <footer className="bg-primaryRed col-start-1 col-end-5 row-span-3">
+        ESTE ES EL FOOTER
+      </footer>
     </div>
   )
 }
