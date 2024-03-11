291063667b6b651b80879ea0a2f91170a4a66f8f
Julio Rodríguez <julio10489@gmail.com>
2024-03-11 14:39:45 -0500

    Changed: Buff style

--- src/components/NavegationMenuHeader.tsx
+++ src/components/NavegationMenuHeader.tsx
@@ -1,32 +1,15 @@
 import { HTMLAttributes } from "react"
+import { SearchIcon } from "./ui/icons/SearchIcon"
 
 interface Props extends HTMLAttributes<HTMLElement> {}
 
 export const NavegationMenuHeader: React.FC<Props> = ()=>{
   return (
-    <nav
-    className="relative flex w-full flex-wrap items-center justify-center bg-zinc-50 py-2 shadow-dark-mild dark:bg-neutral-700 lg:py-4">
-    <div className="ms-5 flex w-[80%] items-center justify-between">
-      <input
-        type="search"
-        className="relative m-0 block w-[1px] min-w-0 flex-auto rounded-l-2xl border border-solid border-secondary-500 bg-transparent bg-clip-padding px-3 py-1.5 text-base font-normal text-surface transition duration-300 ease-in-out focus:border-primary focus:text-gray-700 focus:shadow-inset focus:outline-none motion-reduce:transition-none dark:border-white/10 dark:bg-body-dark dark:text-white dark:placeholder:text-neutral-300 dark:autofill:shadow-autofill"
-        placeholder="Search"
-        aria-label="Search"
-        aria-describedby="button-addon2" />
-      <span
-        className="flex items-center whitespace-nowrap rounded-r-2xl px-3 py-1.5 text-center text-base font-normal text-gray-600 dark:text-white [&>svg]:w-5 bg-primaryRed"
-        id="basic-addon2">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-          fill="currentColor">
-          <path
-            fill-rule="evenodd"
-            d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
-            clip-rule="evenodd" />
-        </svg>
-      </span>
-    </div>
-  </nav>
+    <nav className="w-full h-10 px-10 flex flex-row text-black">
+      <input className="w-full border-none outline-none rounded-l-3xl px-4 bg-bgLight focus:bg-white text-balck" type="text" placeholder="Buscar productos"/>
+      <button className="rounded-r-3xl bg-primaryRed px-4 text-zinc-50 hover:bg-tomato">
+        <SearchIcon />
+      </button>
+    </nav>
   )
 }
