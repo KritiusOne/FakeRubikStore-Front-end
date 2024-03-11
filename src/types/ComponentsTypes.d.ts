ee1e122eda5340308b669c6e3ade7ee962c350a9
Julio Rodríguez <julio10489@gmail.com>
2024-03-11 14:42:35 -0500

    Changed: props from reusable components

--- src/types/ComponentsTypes.d.ts
+++ src/types/ComponentsTypes.d.ts
@@ -1,3 +1,19 @@
+import { HTMLAttributes } from "react"
+
 export interface IconsProps{
   className?: string
+}
+interface OptionButtonProps extends HTMLAttributes<HTMLButtonElement> {
+  title: string
+  Icon: React.FC<IconsProps> 
+}
+
+interface CarrouselProps extends HTMLAttributes<HTMLElement>{
+  imgCarrousel: string
+  previous: () => void
+  next: ()=> void
+}
+interface CarrouselItemProps extends HTMLAttributes<HTMLElement>{
+  img: string
+  stylesImg?: string
 }
