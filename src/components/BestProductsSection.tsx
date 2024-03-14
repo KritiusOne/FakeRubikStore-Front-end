32874a8bbb43f0c0a29e887a38d189fef9e389fc
Julio Rodríguez <julio10489@gmail.com>
2024-03-14 17:00:40 -0500

    Adding: Title on section of best products

--- src/components/BestProductsSection.tsx
+++ src/components/BestProductsSection.tsx
@@ -2,7 +2,8 @@ import { CarrouselProducts } from "./ui/CarrouselProducts"
 
 export const BestProductsSection: React.FC = ()=>{
   return (
-    <div>
+    <div className="flex flex-col justify-center items-center gap-2">
+      <h2 className="text-bgLight text-4xl">Mejores productos</h2>
       <CarrouselProducts />
     </div>
   )
