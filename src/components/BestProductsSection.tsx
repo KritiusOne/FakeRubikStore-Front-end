a9500b7aef7b59eb3c4c1d9971af6d94ff1da583
Julio Rodríguez <julio10489@gmail.com>
2024-03-16 20:15:31 -0500

    Changed: Title and styles of responsive desing

--- src/components/BestProductsSection.tsx
+++ src/components/BestProductsSection.tsx
@@ -3,7 +3,7 @@ import { CarrouselProducts } from "./ui/CarrouselProducts"
 export const BestProductsSection: React.FC = ()=>{
   return (
     <div className="flex flex-col justify-center items-center gap-2">
-      <h2 className="text-bgLight text-4xl">Mejores productos</h2>
+      <h2 className="text-bgLight text-2xl md:text-4xl">Productos mas destacados</h2>
       <CarrouselProducts />
     </div>
   )
