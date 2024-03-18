c89987d1a4033a6e2a90ef83faca3e711f994eeb
Julio Rodríguez <julio10489@gmail.com>
2024-03-18 10:39:28 -0500

    Changed: UI changes

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,25 +1,46 @@
-import { HTMLAttributes } from "react"
+import { HTMLAttributes} from "react"
 import { AsideMenu } from "./AsideMenu"
-import { NavegationMenuHeader } from "./NavegationMenuHeader"
+import { SearchBar } from "./SearchBar"
 import { Footer } from "./Footer"
+import { Link } from "react-router-dom"
+import { PUBLIC_ROUTES } from "@/routes/TypesRoutes"
+import { OptionButton } from "./ui/OptionButton"
+import { CartIcon } from "./ui/icons/CartIcon"
+import { FilterIcon } from "./ui/icons/FilterIcon"
+import { HistoryIcon } from "./ui/icons/HistoryIcon"
+import { UserIcon } from "./ui/icons/UserIcon"
+import { WCAIcon } from "./ui/icons/WCAIcon"
 
 interface Props extends HTMLAttributes<HTMLDivElement> {
   children: JSX.Element | JSX.Element[] | string
 }
 export const Layout: React.FC<Props> = ({children, ...otherProps})=>{
+
   return (
-    <div className="bg-bgDark  min-w-screen min-h-screen
-    grid grid-cols-12 grid-rows-layout font-oswald" {...otherProps}>
-      <div className="col-start-1 col-end-4 row-span-full relative z-10">
-        <AsideMenu className="h-screen fixed w-full max-w-[calc(100vw/12*3)] left-0 bg-bgLight" />
-      </div>
-      <main className="col-start-4 col-end-13 row-start-1 row-end-3 flex flex-col justify-between items-center z-20 py-7">
-        <NavegationMenuHeader />
+    <div className="bg-bgDark min-w-screen min-h-screen
+    flex flex-col justify-center items-start font-oswald" {...otherProps}>
+      <header className="w-full bg-bgLight sticky top-0 z-30">
+        <SearchBar />
+        <ul className="flex flex-row justify-center items-center">
+          <li>
+            <OptionButton title="WCA" Icon={WCAIcon} />
+          </li>
+          <li>
+            <OptionButton title="Busqueda avanzada" Icon={FilterIcon} />
+          </li>
+          <li>
+            <OptionButton title="Historial de compras" Icon={HistoryIcon} />
+          </li>
+        </ul>
+      </header>
+      <main className="flex flex-col justify-between items-center z-20 py-7">
           {
             children
           }
       </main>
-      <Footer className="col-start-1 col-end-13 row-start-3 row-end-4 z-30" />
+      <div className="w-full z-30" >
+        <Footer className="w-full h-full"/>
+      </div>
     </div>
   )
 }
