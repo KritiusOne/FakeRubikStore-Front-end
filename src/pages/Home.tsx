bd28f4cb44b386541777c67e44aac0527c13018c
Julio Rodríguez <julio10489@gmail.com>
2024-03-19 11:13:09 -0500

    Changed: Responsive desing and grid layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,12 +12,12 @@ export const Home: React.FC = ()=>{
   }, [])
   return (
     <Layout>
-      <main className="p-b-5 px-4 gap-10 flex flex-col justify-around items-center">
+      <main className="w-full p-b-5 px-4 gap-10 flex flex-col justify-around items-center">
         <Hero />
         <BestProductsSection /> 
-        <div className="flex flex-col justify-center items-center gap-2">
-          <h2 className="text-2xl md:text-4xl text-bgLight font-bold">Cubos de Rubik</h2>
-          <div className="Home__products--section">
+        <div className="flex flex-col justify-center gap-2">
+          <h2 className="text-2xl md:text-4xl text-center text-bgLight font-bold">Cubos de Rubik</h2>
+          <div className="Home__products--section w-full ">
             {
               products.AllProducts.map((product)=> <CardProduct title={product.name} price={product.price} originalPrice={product.price} priceOff={0} categories={"no"} thumbnail={product.thumbnail} />)
             }
