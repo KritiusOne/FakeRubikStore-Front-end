8192c7e101b720821018e67c31ede6c19a2e7248
Julio Rodríguez <julio10489@gmail.com>
2024-03-30 18:05:00 -0500

    Changed: Properties with errors of writing in carrousels components

--- src/components/ui/CarrouselProducts.tsx
+++ src/components/ui/CarrouselProducts.tsx
@@ -66,7 +66,7 @@ export const CarrouselProducts: React.FC<CarrouselProductsProps> = ({ ...props }
       <button type="button" className="z-30 flex items-center justify-center px-4  cursor-pointer group focus:outline-none" data-carousel-prev onClick={previous}>
         <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
           <svg className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-            <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M5 1 1 5l4 4" />
+            <path stroke="currentColor" strokeLinecap="round" strokeWidth="2" d="M5 1 1 5l4 4" />
           </svg>
           <span className="sr-only">Previous</span>
         </span>
@@ -74,10 +74,10 @@ export const CarrouselProducts: React.FC<CarrouselProductsProps> = ({ ...props }
       <div {...props} className="flex flex-col relative w-full overflow-hidden">
         <div className={`flex flex-row justify-center items-center gap-3 overflow-hidden`}>
         {
-            ProductSelected.length != 0 ? ProductSelected.map((product) => <CardProduct title={product.name} categories={"NO"} originalPrice={product.price} price={product.price} priceOff={0} thumbnail={product.thumbnail} key={product.id} className={`text-primaryRed ${animation == 'right' ? 'slide-in-right' : animation == 'left' ? 'slide-in-left' : ''}`} />) 
+            ProductSelected.length != 0 ? ProductSelected.map((product, i) => <CardProduct title={product.name} categories={"NO"} originalPrice={product.price} price={product.price} priceOff={0} thumbnail={product.thumbnail} key={i} className={`text-primaryRed ${animation == 'right' ? 'slide-in-right' : animation == 'left' ? 'slide-in-left' : ''}`} />) 
             : 
             productsCarousel.map((product, i) => indexSelected.includes(i) ?
-            <CardProduct title={product.name} categories={"NO"} originalPrice={product.price} price={product.price} priceOff={0} thumbnail={product.thumbnail} key={product.id} className={`text-primaryRed `} /> : null)
+            <CardProduct title={product.name} categories={"NO"} originalPrice={product.price} price={product.price} priceOff={0} thumbnail={product.thumbnail} key={i} className={`text-primaryRed `} /> : null)
           }
         </div>
 
@@ -85,7 +85,7 @@ export const CarrouselProducts: React.FC<CarrouselProductsProps> = ({ ...props }
       <button type="button" className="z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next onClick={next}>
         <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
           <svg className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-            <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="m1 9 4-4-4-4" />
+            <path stroke="currentColor" strokeLinecap="round" strokeWidth="2" d="m1 9 4-4-4-4" />
           </svg>
           <span className="sr-only">Next</span>
         </span>
