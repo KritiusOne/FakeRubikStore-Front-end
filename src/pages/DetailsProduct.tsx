94f5c2987424802047212a2ae3729db8f43f1af1
Julio Rodríguez <julio10489@gmail.com>
2024-07-08 19:25:06 -0500

    Cambiado: Diseño e implementacion de los llamados a base de datos, debido a las mejoras en la API

--- src/pages/DetailsProduct.tsx
+++ src/pages/DetailsProduct.tsx
@@ -1,48 +1,45 @@
 import { Layout } from "@/components/Layout"
 import { Button } from "@/components/ui/Button"
-import { Product } from "@/types/ProductsTypes"
+import { CartIcon } from "@/components/ui/icons/CartIcon"
+import { AllDataProduct } from "@/types/ProductsTypes"
 import { ResponseBase } from "@/types/ResponseTypes"
 import { useEffect, useState } from "react"
 import { useLocation } from "react-router-dom"
 
 export const DetailsProduct: React.FC = () => {
   const location = useLocation()
-  const [title, setTitle] = useState<string | null>("")
-  const [Description, setDescription] = useState<string | null>("")
-  const [Img, setImg] = useState<string | null>("")
-  //const [Thumbnail, setThumbnail] = useState<string | null>("")
-  const [Price, setPrice] = useState<string | null>("")
   const params = new URLSearchParams(location.search)
+  const [productDatails, setProductsDetails] = useState<AllDataProduct>()
   useEffect(() => {
-    const getImages = async () => {
+    const getProduct = async () => {
       const URL = `${import.meta.env.VITE_API_URL_PRODUCT_BY_ID}${params.get("id")}`
       const response = await fetch(URL)
+      console.log(response)
       if (response.ok) {
-        const productData: ResponseBase<Product> = await response.json()
-        setImg(productData.response.image)
+        const productData: ResponseBase<AllDataProduct> = await response.json()
+        setProductsDetails(productData.response)
       }
     }
-    const newTitle = typeof (params.get("title")) == "string" ? params.get("title") : "Cubo Rubik"
-    setTitle(newTitle)
-    setDescription(typeof (params.get("description")) == "string" ? params.get("description") : "Cubo Rubik")
-    setPrice(typeof (params.get("price")) == "string" ? params.get("price") : "Cubo Rubik")
-    getImages()
+    getProduct()
   }, [])
   return (
     <Layout className="flex justify-center items-center">
-      <main className="flex flex-col md:grid md:grid-cols-2 w-full gap-6 justify-center items-center">
-        <section className="w-full flex justify-center">
-          <img src={Img?.toString()} className="aspect-square" alt="Imagen del producto" />
+      <main className="flex flex-col md:grid md:grid-cols-3 justify-center items-center lg:min-w-[800px] max-w-full px-6 py-4 bg-bgLight text-bgDark mx-6">
+        <section>
+          <img src={productDatails?.image} alt={`Imagen del ${productDatails?.name}`} />
         </section>
-        <section className="w-full text-left flex flex-col justify-start items-start gap-4 text-bgLight px-4">
-          <h2 className="font-bold text-3xl w-full"> {title} </h2>
-          <strong className="text-xl"> $ {Price} </strong>
-          <p className="text-balance max-w-[80ch]">
-            {
-              Description
-            }
+        <section className="flex flex-col justify-center items-center px-4">
+          <h1 className="text-pretty text-3xl"> {productDatails?.name} </h1>
+          <strong className="text-xl"> {productDatails?.price} </strong>
+          <span> Aqui van las calificaciones </span>
+          <p className="text-balance">
+            {productDatails?.description}
           </p>
-          <Button title="Agregar al Carrito" className="max-w-full flex text-center text-md rounded-md px-3 justify-between gap-2 items-center bg-primaryRed hover:bg-green transition-colors ease-in-out" />
+        </section>
+        <section className="flex flex-col justify-center items-center gap-4">
+          <span>Stock disponible: {productDatails?.stock} </span>
+          <Button size="extraLarge" className="flex flex-row justify-center items-center gap-2 text-lg"> <span>Agregar al carrito</span> <CartIcon /> </Button>
+          <Button primary={true} size="extraLarge" className="flex flex-row justify-center items-center gap-2 text-lg "> <span>Comprar ahora</span> <CartIcon /> </Button>
         </section>
       </main>
     </Layout>
