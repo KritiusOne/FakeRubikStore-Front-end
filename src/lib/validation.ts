90bbb1025c86d82a9e5dd023b28dae5244d2cbc9
Julio Rodríguez <julio10489@gmail.com>
2024-07-16 20:39:56 -0500

    Agregado: RegEx para validar los registros

---
+++ src/lib/validation.ts
@@ -0,0 +1,3 @@
+export const nameRegEx =  /^[a-zA-Z]{3,30}$/
+export const emailRegEx = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+export const passwordRegEx = /^(?=.*[a-z])(?=.*[0-9])(?=.{8,}$)[a-zA-Z0-9!@#$%^&*()_+=-]{8,}$/;
