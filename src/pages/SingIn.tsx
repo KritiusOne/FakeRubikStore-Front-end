8daffd5e2b9e190202b12e8f2d3044f71e8ab7f3
Julio Rodríguez <julio10489@gmail.com>
2024-07-16 20:40:51 -0500

    Agregado: primera implementacion de validacion con el manejo grafico

--- src/pages/SingIn.tsx
+++ src/pages/SingIn.tsx
@@ -4,42 +4,85 @@ import { useState } from "react"
 import cover from "@/assets/cover-login-2.avif"
 import { Label } from "@/components/ui/Label"
 import { Input } from "@/components/ui/Input"
+import { emailRegEx, nameRegEx, passwordRegEx } from "@/lib/validation"
 
+interface SignInField {
+  value: string
+  state: "success" | "error" | "normal"
+}
 export const SingIn: React.FC = () => {
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [secondPassword, setSecondPassword] = useState("")
-  const [name, setName] = useState("")
-  const [lastName, setLastName] = useState("")
-
+  const [name, setName] = useState<SignInField>({
+    state: "normal",
+    value: ""
+  })
+  const [lastName, setLastName] = useState<SignInField>({
+    state: "normal",
+    value: ""
+  })
+  const [email, setEmail] = useState<SignInField>({
+    state: "normal",
+    value: ""
+  })
+  const [password, setPassword] = useState<SignInField>({
+    state: "normal",
+    value: ""
+  })
+  const [secondPassword, setSecondPassword] = useState<SignInField>({
+    state: "normal",
+    value: ""
+  })
+  
+  const handleChangeNames = (e: React.ChangeEvent<HTMLInputElement>)=>{
+    nameRegEx.test(e.target.value) ? setName({state: "success", value: e.target.value}) : setName({state: "error", value: e.target.value}) 
+  }
+  const handleChangeLastName = (e: React.ChangeEvent<HTMLInputElement>)=>{
+    nameRegEx.test(e.target.value) ? setLastName({
+      state: "success",
+      value: e.target.value
+    }) : setLastName({
+      state: "error",
+      value: e.target.value
+    })
+  }
+  const handleChangeEmail = (e: React.ChangeEvent<HTMLInputElement>)=>{
+    emailRegEx.test(e.target.value.toLowerCase()) ? setEmail({state: "success", value: e.target.value.toLocaleLowerCase()}) : setEmail({state: "error", value: e.target.value.toLocaleLowerCase()})
+    console.log(e.target.value.toLowerCase())
+  }
+  const handleChangePasword = (e: React.ChangeEvent<HTMLInputElement>)=>{
+    passwordRegEx.test(e.target.value) ? setPassword({state: "success", value: e.target.value}) : setPassword({state: "error", value: e.target.value})
+  }
+  
+  const handleChangeSecondPasword = (e: React.ChangeEvent<HTMLInputElement>)=>{
+    passwordRegEx.test(e.target.value) ? setSecondPassword({state: "success", value: e.target.value}) : setSecondPassword({state: "error", value: e.target.value})
+  }
   return (
     <Layout className="w-full flex flex-col justify-between items-center text-bgLight">
       <main className="flex flex-row w-full justify-between items-center">
-        <div className="w-full h-full flex flex-col justify-center items-center px-6 py-8 gap-2">
+        <div className="w-full h-screen md:h-full flex flex-col justify-center items-center px-6 py-8 gap-2">
           <div className="flex flex-col justify-center items-start gap-0.5 w-3/4">
             <h2 className='text-4xl text-center text-bgLight'>Registrarse</h2>
             <Label>Ingrese su informacion</Label>
           </div>
           <form className="flex flex-col justify-center items-center w-full gap-2">
             <div className="w-3/4 text-left flex flex-col justify-center items-start gap-0.5">
-              <Label htmlFor="name"> Nombre </Label>
-              <Input id="name" stateData="normal" value={name} onChange={(e) => setName(e.target.value)} className='w-full outline-none rounded-3xl px-4 bg-bgLight focus:bg-white text-balck border-2 text-black' type="text" placeholder="Jonh" />
+              <Label stateData={name.state} htmlFor="name"> Nombre </Label>
+              <Input id="name" stateData={name.state} value={name.value} onChange={handleChangeNames} className='w-full outline-none rounded-3xl px-4 bg-bgLight focus:bg-white text-balck border-2 text-black' type="text" placeholder="Jonh" />
             </div>
             <div className="w-3/4 text-left flex flex-col justify-center items-start gap-0.5">
-              <Label> Apellido </Label>
-              <Input stateData="normal" placeholder="Doe" value={lastName} onChange={(e) => setLastName(e.target.value)} type="text" />
+              <Label stateData={lastName.state} htmlFor="lastname"> Apellido </Label>
+              <Input id="lastname" stateData={lastName.state} placeholder="Doe" value={lastName.value} onChange={handleChangeLastName} type="text" />
             </div>
             <div className="w-3/4 text-left flex flex-col justify-center items-start gap-0.5">
-              <Label> Correo Electronico </Label>
-              <Input stateData="normal" placeholder="mail@example.com" value={email} onChange={(e) => setEmail(e.target.value)} type="text" />
+              <Label htmlFor="mail" stateData={email.state}> Correo Electronico </Label>
+              <Input stateData={email.state} id="mail" placeholder="mail@example.com" value={email.value} onChange={handleChangeEmail} type="text" />
             </div>
             <div className="w-3/4 text-left flex flex-col justify-center items-start gap-0.5">
-              <Label> Contraseña </Label>
-              <Input stateData="normal" placeholder="min 8 caracteres" value={password} onChange={(e) => setPassword(e.target.value)}type="password" />
+              <Label stateData={password.state} htmlFor="password"> Contraseña </Label>
+              <Input stateData={password.state} id="password" placeholder="min 8 caracteres" value={password.value} onChange={handleChangePasword}type="password" />
             </div>
             <div className="w-3/4 text-left flex flex-col justify-center items-start gap-0.5">
-              <Label> Confirmar contraseña </Label>
-              <Input stateData="normal" placeholder="min 8 caracteres" value={secondPassword} onChange={(e) => setSecondPassword(e.target.value)} className='w-full outline-none rounded-3xl px-4 bg-bgLight focus:bg-white text-balck border-2 text-black' type="password" />
+              <Label htmlFor="second_password" stateData={secondPassword.state}> Confirmar contraseña </Label>
+              <Input stateData={secondPassword.state} id="second_password" placeholder="min 8 caracteres" value={secondPassword.value} onChange={handleChangeSecondPasword} className='w-full outline-none rounded-3xl px-4 bg-bgLight focus:bg-white text-balck border-2 text-black' type="password" />
             </div>
             <Button size="extraLarge" primary={true} onClick={() => console.log("SI PRESIONE")}> <span>Registrarse</span> </Button>
           </form>
