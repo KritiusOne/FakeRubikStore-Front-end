68aed4c8d589a6b0e781cd2a63fd9846620966c5
Julio Rodríguez <julio10489@gmail.com>
2024-07-18 11:17:52 -0500

    Agregada: Funcion Add

--- src/zustand/CartStorage.ts
+++ src/zustand/CartStorage.ts
@@ -5,6 +5,7 @@ interface Cart {
   ProductsCart: ProductInCart[]
   viewCart: boolean
   changeViewCart: (view: boolean)=>void
+  add: (numberSol:number, id: number, nameProd: string, price: number, img: string)=>void
 }
 export const useCartStorage = create<Cart>((set, get)=>({
   ProductsCart: [],
@@ -12,5 +13,14 @@ export const useCartStorage = create<Cart>((set, get)=>({
   changeViewCart: (view)=>{
     const cartStorage = get()
     set({...cartStorage, viewCart: !view})
+  },
+  add: (numberSol, id, nameProd, price, img)=>{
+    const cartStorage = get()
+    const copy = [...cartStorage.ProductsCart]
+    const finded = copy.find((product)=> product.id == id)
+    if(finded == undefined){
+      copy.push({id: id, name: nameProd, numberProd: numberSol, price: price, thumbnail: img})
+      set({viewCart: cartStorage.viewCart, ProductsCart: copy})
+    }
   }
 }))
