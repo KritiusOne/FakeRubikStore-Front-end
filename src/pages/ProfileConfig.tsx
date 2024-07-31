8a0d082393f3cd0766abcf92d35520af9891b6c4
Julio Rodríguez <julio10489@gmail.com>
2024-07-31 15:59:57 -0500

    Agregado: Profile Config page, que se terminará después

---
+++ src/pages/ProfileConfig.tsx
@@ -0,0 +1,11 @@
+import { Layout } from '@/components/Layout'
+
+export const ProfileConfig = () => {
+  return (
+    <Layout>
+      <main>
+        <h1>Profile</h1>
+      </main>
+    </Layout>
+  )
+}
--- src/routes/MyRoutes.tsx
+++ src/routes/MyRoutes.tsx
@@ -7,6 +7,7 @@ import { NotFount } from "@/pages/NotFound"
 import { DetailsProduct } from "@/pages/DetailsProduct"
 import { Auth_Guard } from "./Auth.guard"
 import { MakeBuy } from "@/pages/MakeBuy"
+import { ProfileConfig } from "@/pages/ProfileConfig"
 export const MyRoutes: React.FC<{}> = () => {
   return (
     <BrowserRouter>
@@ -17,6 +18,9 @@ export const MyRoutes: React.FC<{}> = () => {
         <Route path={PUBLIC_ROUTES.VIEW_PRODUCT} element={<DetailsProduct />} />
         <Route element={<Auth_Guard />} >
           <Route path={PRIVATE_USER_ROUTES.MAKE_BUY} element={<MakeBuy />} />
+        </Route>
+        <Route element={<Auth_Guard />} >
+          <Route path={PRIVATE_USER_ROUTES.PROFILE_CONFIG} element={<ProfileConfig />} />
         </Route>
         <Route path={PUBLIC_ROUTES[404]} element={<NotFount />} />
       </Routes>
