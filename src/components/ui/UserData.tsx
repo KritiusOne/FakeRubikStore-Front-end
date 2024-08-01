3de97b4a6d3dfeed6391545ada74b76cc08fa23f
Julio Rodríguez <julio10489@gmail.com>
2024-08-01 18:04:14 -0500

    Cambiado: Cambios en UserData, el boton no es necesario

--- src/components/ui/UserData.tsx
+++ src/components/ui/UserData.tsx
@@ -1,13 +1,11 @@
 import { Icon, IconProps } from "@tabler/icons-react"
-import { Button } from "./Button"
 
 interface Props {
   title: string
   Icon: React.ForwardRefExoticComponent<IconProps & React.RefAttributes<Icon>>
   info: string
-  onClickButton: ()=> void
 }
-export const UserData: React.FC<Props> = ({Icon, info, title, onClickButton})=>{
+export const UserData: React.FC<Props> = ({Icon, info, title})=>{
   return (
     <article className="w-full flex flex-row justify-between items-center shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] rounded-lg px-2 py-1">
       <div className="flex flex-row justify-center items-center gap-3">
@@ -19,7 +17,6 @@ export const UserData: React.FC<Props> = ({Icon, info, title, onClickButton})=>{
           </p>
         </div>
       </div>
-      <Button className="text-[12px] text-center" onClick={onClickButton} primary={false} size="large"> Cambiar </Button>
     </article>
   )
 }
