16a4f22493a8d516c0fea3e3f3ec4b320347c9c8
Julio Rodríguez <julio10489@gmail.com>
2024-08-03 09:08:28 -0500

    Agregado: Boton de log out

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,13 +3,17 @@ import { HMenu } from "./ui/icons/HMenu";
 import { SearchBar } from "./SearchBar";
 import { WCAIcon } from "./ui/icons/WCAIcon";
 import { Button } from "./ui/Button";
-import { IconFilterFilled } from '@tabler/icons-react';
+import { IconFilterFilled, IconLogout } from '@tabler/icons-react';
 import { IconHistory } from '@tabler/icons-react';
 import { useUserSesion } from "@/zustand/UserStorage";
+import { useNavigate } from "react-router-dom";
+import { PUBLIC_ROUTES } from "@/routes/TypesRoutes";
 
 interface Props extends HTMLAttributes<HTMLElement>{}
 export const Header: React.FC<Props> = ({...props})=> {
   const activeSesion = useUserSesion(Storage => Storage.activeSesion)
+  const logOut = useUserSesion(Storage => Storage.logOut)
+  const navegate = useNavigate()
   return (
     <header {...props} className={`w-full bg-bgLight sticky top-0 z-30 flex flex-row justify-between items-center py-3 px-4 md:flex-col md:justify-center overflow-hidden ${props.className}`}>
         <HMenu className="block md:hidden cursor-pointer text-primaryRed" />
@@ -24,6 +28,15 @@ export const Header: React.FC<Props> = ({...props})=> {
           {
             activeSesion && <li>
             <Button size="extraLarge" className="flex flex-row gap-2 text-primaryRed border-primaryRed hover:bg-bgDark hover:text-white hover:border-bgDark"><span>Historial de compras</span> <IconHistory /> </Button>
+          </li>
+          }
+          {
+            activeSesion && <li>
+            <Button size="extraLarge" className="flex flex-row gap-2 text-primaryRed border-primaryRed hover:bg-bgDark hover:text-white hover:border-bgDark"
+            onClick={()=>{
+              logOut()
+              navegate(PUBLIC_ROUTES.HOME)
+            }}><span>Cerrar sesión</span> <IconLogout /> </Button>
           </li>
           }
         </ul>
