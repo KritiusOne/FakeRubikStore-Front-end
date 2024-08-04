616cd905fa994d9faed4909a0592c9261fab016b
Julio Rodríguez <julio10489@gmail.com>
2024-08-04 13:36:40 -0500

    Agregado: Nuevo Endpoint para actualizar el usuario

--- src/zustand/URLStorage.ts
+++ src/zustand/URLStorage.ts
@@ -4,11 +4,15 @@ interface URLStorageTypes {
   Products: string 
   Login: string
   GetAddressById: (id:string) => string
+  UpdateUser: (id: string) => string
 }
 export const useURLStorage = create<URLStorageTypes>(()=>({
   Products: import.meta.env.VITE_API_URL_PRODUCTS,
   Login: `${import.meta.env.VITE_API_URL}api/Login`,
   GetAddressById: (id)=>{
     return `${import.meta.env.VITE_API_URL_GET_ADDRESS_BY_ID}${id}`
+  },
+  UpdateUser: (id) => {
+    return `${import.meta.env.VITE_API_URL_UPDATE_USER}${id}`
   }
 }))
