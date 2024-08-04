4d3f76477f2a5998c613f9ebeb56ee0d78c0e9af
Julio Rodríguez <julio10489@gmail.com>
2024-08-04 13:34:57 -0500

    Cambiado: Cambio en la validación del numero

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -1,4 +1,4 @@
 export const nameRegEx =  /^[a-zA-Z]{3,30}$/
 export const emailRegEx = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
 export const passwordRegEx = /^(?=.*[a-z])(?=.*[0-9])(?=.{8,}$)[a-zA-Z0-9!@#$%^&*()_+=-]{8,}$/;
-export const onlyNumbersRegEx = /\d+$/
+export const onlyNumbersRegEx = /\d{10}$/
