a93879d6e62757f1c498be4e1d1f7bffe011a628
Julio Rodríguez <julio10489@gmail.com>
2024-08-09 21:18:53 -0500

    Cambiado: Cambios en la renderización de la información, ahora si se renderiza información correctamente

--- src/pages/ShoppingHistory.tsx
+++ src/pages/ShoppingHistory.tsx
@@ -1,6 +1,7 @@
+import { CardProductHistory } from '@/components/CardProductHistory'
 import { Layout } from '@/components/Layout'
 import { Spinner } from '@/components/ui/Spinner'
-import { BasicOrderInfo } from '@/types/OrdersTypes'
+import { Order } from '@/types/OrdersTypes'
 import { PaginatedResponse } from '@/types/ResponseTypes'
 import { useURLStorage } from '@/zustand/URLStorage'
 import { useUserSesion } from '@/zustand/UserStorage'
@@ -10,7 +11,7 @@ export const ShoppingHistory: React.FC = () => {
   const [load, setLoad] = useState(false)
   const UserSesion = useUserSesion()
   const GetURL = useURLStorage(Storage => Storage.GetAllOrdersByUser)
-  const [PaginatedOrdersByUser, setPaginatedOrdersByUser] = useState<PaginatedResponse<BasicOrderInfo[]>>()
+  const [PaginatedOrdersByUser, setPaginatedOrdersByUser] = useState<PaginatedResponse<Order[]>>()
   const [pageOrders, setPageOrders] = useState(1)
   useEffect(()=>{
     const getAllOrdersByUser = async()=>{
@@ -27,7 +28,7 @@ export const ShoppingHistory: React.FC = () => {
           }
         })
         if(res.ok){
-          const response:PaginatedResponse<BasicOrderInfo[]> = await res.json()
+          const response:PaginatedResponse<Order[]> = await res.json()
           setPaginatedOrdersByUser(response)
           setLoad(false)
         }
@@ -39,13 +40,24 @@ export const ShoppingHistory: React.FC = () => {
   }, [])
   return (
     <Layout>
-      <main className='bg-bgLight w-screen h-full my-10 flex flex-col justify-center items-center'>
+      <main className='bg-bgLight w-11/12 h-full my-10 flex flex-col justify-start items-center gap-3 box-border py-5'>
+        <h1 className='text-3xl font-bold text-center text-pretty'> Historial de compras </h1>
+        <div>
+          {
+            load && <Spinner />
+          }
+        </div>
+        <div className='w-10/12 px-4 py-3 flex flex-col justify-center items-center gap-4'>
         {
-          load && <Spinner />
-        }
-        {
-          PaginatedOrdersByUser != undefined && <span className='max-w-[100ch] '> {JSON.stringify(PaginatedOrdersByUser)} </span>
-        }
+            !load && PaginatedOrdersByUser != undefined && PaginatedOrdersByUser.response.map((OrderOfUser)=> <CardProductHistory 
+            dateBuy={OrderOfUser.date} 
+            nameProduct={OrderOfUser.orderProducts[0].productInfo.name} 
+            stateDelivery={OrderOfUser.deliveryInfo.idState} 
+            key={OrderOfUser.id}
+            img={OrderOfUser.orderProducts[0].productInfo.thumbnail} 
+            allProductInOrder={OrderOfUser.orderProducts}/>)
+          }
+        </div>
       </main>
     </Layout>
   )
