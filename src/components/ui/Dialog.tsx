11f307810a160202bdfcf76b78c425c1e42b23cd
Julio Rodríguez <julio10489@gmail.com>
2024-08-12 12:48:58 -0500

    Agregado: Componente Dialog para hacer modales

---
+++ src/components/ui/Dialog.tsx
@@ -0,0 +1,19 @@
+interface Props {
+  children: React.ReactNode
+  onClose: () => void
+}
+
+export const Dialog: React.FC<Props> = ({ children, onClose }) => {
+  const handleMainClick = (e: React.MouseEvent) => {
+    e.stopPropagation()
+  }
+  return (
+    <div onClick={() => onClose()} className='w-screen h-screen fixed top-0 left-0 flex justify-center items-center md:justify-center bg-black/[0.5] z-[300]'>
+      <main className='h-11/12 w-11/12 text-dark z-[1000] flex flex-col justify-center items-center' onClick={handleMainClick} >
+        {
+          children
+        }
+      </main>
+    </div>
+  )
+}
