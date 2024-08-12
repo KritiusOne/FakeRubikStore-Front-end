b2a94f5f29ef627f7f64faddb9d8987da96421d1
Julio Rodríguez <julio10489@gmail.com>
2024-08-12 13:58:40 -0500

    Cambiado: Cambios en los litados de links, agregado el del repo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,31 +1,47 @@
 import { HTMLAttributes } from "react"
-import { ListContainer } from "./ui/ListContainer"
-import { ExternalLink } from "./ui/ExternalLink"
 import { LinkedIn } from "./ui/icons/LinkedIn"
 import { Github } from "./ui/icons/Github"
 import { Instagram } from "./ui/icons/Instagram"
 
 interface Props extends HTMLAttributes<HTMLElement> { }
-export const Footer: React.FC<Props> = ({...props}) => {
+export const Footer: React.FC<Props> = ({ ...props }) => {
   return (
     <footer {...props} className={`bg-primaryRed grid grid-cols-1 md:grid-cols-3 grid-rows-1 py-10 ${props.className}`}>
-      <ListContainer title="Resource">
+      <ul className={`flex flex-col justify-center items-start gap-2 px-10 text-bgLight`} >
+        <h4 className="font-oswald font-bold text-xl">
+          Resource
+        </h4>
         <li>Documentación API</li>
         <li>Blog</li>
-        <li>Repositorio</li>
-      </ListContainer>
-      <ListContainer title="Info">
-          <li>About Us</li>
-          <li>Portafolio</li>
-          <li>Preguntas y respuestas</li>
-      </ListContainer>
-      <ListContainer title="Follow me">
-          <li className="flex flex-row gap-4">
-            <ExternalLink Icon={LinkedIn} linkTo="https://www.linkedin.com/in/juliorodriguezureche--/" />
-            <ExternalLink Icon={Github} linkTo="https://github.com/KritiusOne" />
-            <ExternalLink Icon={Instagram} linkTo="https://www.instagram.com/rour10/" />
-          </li>
-      </ListContainer>
+        <li>
+          <a target="_blank" href="https://github.com/KritiusOne/FakeRubikStore-Front-end" className="">
+            Repositorio del proyecto
+          </a>
+        </li>
+      </ul>
+      <ul className={`flex flex-col justify-center items-start gap-2 px-10 text-bgLight`} >
+        <h4 className="font-oswald font-bold text-xl">
+          Info
+        </h4>
+        <li>About Us</li>
+        <li>Portafolio</li>
+      </ul>
+      <ul className={`flex flex-col justify-center items-start gap-2 px-10 text-bgLight`} >
+        <h4 className="font-oswald font-bold text-xl">
+          Follow me
+        </h4>
+        <li className="flex flex-row gap-4">
+          <a target="_blank" href="https://www.linkedin.com/in/juliorodriguezureche--/" >
+            <LinkedIn className="w-8 h-8" />
+          </a>
+          <a target="_blank" href="https://github.com/KritiusOne" >
+            <Github className="w-8 h-8" />
+          </a>
+          <a target="_blank" href="https://www.instagram.com/rour10/" >
+            <Instagram className="w-8 h-8" />
+          </a>
+        </li>
+      </ul>
     </footer>
   )
 }
