4753bde1158fc77b2f506b13fc5ed6663befbbb5
Julio Rodríguez <julio10489@gmail.com>
2024-09-07 19:32:26 -0500

    Agregado: Maquetacion de como se va a ver el panel de control

--- src/pages/PanelControl.tsx
+++ src/pages/PanelControl.tsx
@@ -1,20 +1,21 @@
 import { Layout } from '@/components/Layout'
+import { TabsCollection } from '@/components/TabsCollection'
+import { Tab } from '@/components/ui/Tab'
 import React from 'react'
 
-interface Props{
+interface Props{}
 
-}
 export const PanelControl: React.FC<Props> = () => {
   return (
     <Layout>
-      <div className=''>
-        <aside>
-          Este es el aside
-        </aside>
-        <main>
-          este es el main
-        </main>
-      </div>
+      <main className='w-full h-full my-10 bg-bgLight flex flex-col justify-center items-center px-2 py-3 gap-2'>
+        <h1 className='text-3xl font-semibold text-pretty text-center'>Panel de control</h1>
+        <TabsCollection>
+          <Tab TabTitle='Productos' state={false} />
+          <Tab TabTitle='Usuarios' state={false} />
+          <Tab TabTitle='Ventas' state={false} />
+        </TabsCollection>
+      </main>
     </Layout>
   )
 }
