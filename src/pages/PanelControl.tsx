60590ea3789dd2de3bd5bf6a61e328380138b3c1
Julio Rodríguez <julio10489@gmail.com>
2024-09-13 19:02:16 -0500

    Agregado: Implementación del helper en el infinite scroll de los productos e implementación del infinite scroll con el helper de las ordenes

--- src/pages/PanelControl.tsx
+++ src/pages/PanelControl.tsx
@@ -2,25 +2,33 @@ import { Layout } from '@/components/Layout'
 import { TabsCollection } from '@/components/TabsCollection'
 import { Spinner } from '@/components/ui/Spinner'
 import { Tab } from '@/components/ui/Tab'
+import { getGroupRegisters } from '@/lib/getGroupRegisters'
 import { Order } from '@/types/OrdersTypes'
 import { Product } from '@/types/ProductsTypes'
 import { PaginatedResponse } from '@/types/ResponseTypes'
 import { useURLStorage } from '@/zustand/URLStorage'
+import { useUserSesion } from '@/zustand/UserStorage'
 import React, { lazy, Suspense, useEffect, useRef, useState } from 'react'
 
-interface Props{}
+interface Props { }
 
 type TypesTabs = "Products" | "Users" | "Sells"
 const INITIAL_PAGE_SIZE = 10
 const INITIAL_PAGE_NUMBER = 1
 
-const ProductsDashboard = lazy(()=> import("@/components/ProductsDashboard"))
+const ProductsDashboard = lazy(() => import("@/components/ProductsDashboard"))
+const OrdersDashboard = lazy(() => import("@/components/OrdersDashboard"))
+
+
+
 export const PanelControl: React.FC<Props> = () => {
   const [activeTab, setActiveTab] = useState<TypesTabs>("Products")
   const [load, setLoad] = useState(false)
+  const [LoadOrders, setLoadOrders] = useState(false)
   const [PageSize] = useState(INITIAL_PAGE_SIZE)
-  const [PageNumberProducts, setPageNumber] = useState(INITIAL_PAGE_NUMBER)
-  const URL_BASE = useURLStorage(Storage => Storage.Products)
+  const [PageNumberProducts, setPageNumberProducts] = useState(INITIAL_PAGE_NUMBER)
+  const [PageNumberOrders, setPageNumberOrders] = useState(INITIAL_PAGE_NUMBER)
+  const { AllOrders, Products } = useURLStorage()
   const [InfoProducts, setInfoProducts] = useState<PaginatedResponse<Product[]>>({
     metaData: {
       currentPage: 0,
@@ -36,60 +44,93 @@ export const PanelControl: React.FC<Props> = () => {
     response: [],
     statusCode: 404
   })
-  
+  const [OrdersInfo, setOrdersInfo] = useState<PaginatedResponse<Order[]>>({
+    metaData: {
+      currentPage: 0,
+      hasNextPage: false,
+      hasPreviousPage: false,
+      nextPageURL: "",
+      previousPageURL: "",
+      pageSize: 0,
+      totalCount: 0,
+      totalPage: 0
+    },
+    msg: "",
+    response: [],
+    statusCode: 404
+  })
   const tableProductsRef = useRef<HTMLDivElement>(null)
-  const getProducts = async()=>{
-    if(activeTab != "Products"){
-      return
-    }
-    const params = new URLSearchParams()
-    params.append("PageSize", PageSize.toString())
-    params.append("PageNumber", PageNumberProducts.toString())
-    try {
-      setLoad(true)
-      const res = await fetch(URL_BASE + params.toString())
-      if(res.ok){
-        const response: PaginatedResponse<Product[]> = await res.json()
-        setInfoProducts((prev)=> ({
-          metaData: response.metaData,
-          msg: response.msg,
-          response: [...prev.response, ...response.response],
-          statusCode: response.statusCode
+  const tableOrdersRef = useRef<HTMLDivElement>(null)
+  const { token, typetoken } = useUserSesion()
+  useEffect(() => {
+    if(activeTab != "Products") return
+    setLoad(true)
+    getGroupRegisters<Product[]>(PageSize.toString(), PageNumberProducts.toString(),`${typetoken} ${token}`, Products).then(res =>{
+      if(res != undefined){
+        setInfoProducts(prev => ({
+          metaData: {
+            ...res.metaData,
+            totalCount: res.metaData.totalCount + prev.metaData.totalCount,
+          },
+          msg: res.msg,
+          response: [...prev.response, ...res.response],
+          statusCode: res.statusCode
         }))
-        setLoad(false)
+        
       }
-    } catch (error) {
-      console.log(error)
-    }
-  }
+    }).finally(()=> setLoad(false))
+  }, [PageNumberProducts, activeTab])
+  useEffect(() => {
+    if(activeTab != "Sells") return
+    setLoadOrders(true)
+    getGroupRegisters<Order[]>("5", PageNumberOrders.toString(),`${typetoken} ${token}`, AllOrders).then(res =>{
+      if(res != undefined){
+        setOrdersInfo(prev => ({
+          metaData: {
+            ...res.metaData,
+            totalCount: res.metaData.totalCount + prev.metaData.totalCount,
+          },
+          msg: res.msg,
+          response: [...prev.response, ...res.response],
+          statusCode: res.statusCode
+        }))
+      }
+    }).finally(()=> setLoadOrders(false))
+    
+  }, [PageNumberOrders, activeTab])
 
-  useEffect(()=>{
-    getProducts()    
-  }, [PageNumberProducts])
-
-  const manageTabs = (id: string)=>{
-    if(id == 'Products'){
+  const manageTabs = (id: string) => {
+    if (id == 'Products') {
       setActiveTab("Products")
       return
     }
-    if(id == "Users"){
+    if (id == "Users") {
       setActiveTab("Users")
       return
     }
-    if(id == "Sells"){
+    if (id == "Sells") {
       setActiveTab("Sells")
       return
     }
   }
-  const OnObserver = async(entries: any[])=>{
-    const first = entries[0]
-    if(first.isIntersecting && InfoProducts.metaData.hasNextPage) setPageNumber(prev => prev + 1)
-  }
-  useEffect(()=>{
-    const observer = new IntersectionObserver(OnObserver)
-    if(!load && observer && tableProductsRef.current) observer.observe(tableProductsRef.current)
+  useEffect(() => {
+    const observer = new IntersectionObserver(async (entries) => {
+      const first = entries[0]
+      if (first.isIntersecting && InfoProducts.metaData.hasNextPage) setPageNumberProducts(prev => prev + 1)
+    })
+    if (!load && observer && tableProductsRef.current) observer.observe(tableProductsRef.current)
     return () => {
-      if(observer) observer.disconnect()
+      if (observer) observer.disconnect()
+    }
+  })
+  useEffect(() => {
+    const observer = new IntersectionObserver(async (entries) => {
+      const first = entries[0]
+      if (first.isIntersecting && OrdersInfo.metaData.hasNextPage) setPageNumberOrders(prev => prev + 1)
+    })
+    if (!LoadOrders && observer && tableOrdersRef.current) observer.observe(tableOrdersRef.current)
+    return () => {
+      if (observer) observer.disconnect()
     }
   })
   return (
@@ -97,9 +138,9 @@ export const PanelControl: React.FC<Props> = () => {
       <main className='w-full h-full my-10 bg-bgLight flex flex-col justify-center items-center px-2 py-3 gap-2'>
         <h1 className='text-3xl font-semibold text-pretty text-center'>Panel de control</h1>
         <TabsCollection>
-          <Tab onClick={(e)=> manageTabs(e.currentTarget.id)} id='Products' TabTitle='Productos' state={activeTab == "Products"} />
-          <Tab onClick={(e)=> manageTabs(e.currentTarget.id)} id='Users' TabTitle='Usuarios' state={activeTab == "Users"} />
-          <Tab onClick={(e)=> manageTabs(e.currentTarget.id)} id='Sells' TabTitle='Ventas' state={activeTab == "Sells"} />
+          <Tab onClick={(e) => manageTabs(e.currentTarget.id)} id='Products' TabTitle='Productos' state={activeTab == "Products"} />
+          <Tab onClick={(e) => manageTabs(e.currentTarget.id)} id='Users' TabTitle='Usuarios' state={activeTab == "Users"} />
+          <Tab onClick={(e) => manageTabs(e.currentTarget.id)} id='Sells' TabTitle='Ventas' state={activeTab == "Sells"} />
         </TabsCollection>
         {
           activeTab == "Products" && (
@@ -108,7 +149,13 @@ export const PanelControl: React.FC<Props> = () => {
             </Suspense>
           )
         }
-        
+        {
+          activeTab == "Sells" && (
+            <Suspense fallback={<Spinner />}>
+              <OrdersDashboard myRef={tableOrdersRef} load={LoadOrders} InfoOrders={OrdersInfo.response} />
+            </Suspense>
+          )
+        }
       </main>
     </Layout>
   )
