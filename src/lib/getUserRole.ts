70cff40cd59bacf2187f2583aa8f273fc0916372
Julio Rodríguez <julio10489@gmail.com>
2024-09-27 17:15:58 -0500

    Agregado: Funcion que me devuelve el idRol por el nombre

--- src/lib/getUserRole.ts
+++ src/lib/getUserRole.ts
@@ -6,3 +6,11 @@ export const ROLE_NAME =[
 export function getUserRole(idRol:number){
   return ROLE_NAME[idRol - 1]
 }
+export function getNumberRole(NameRol:string){
+  if(ROLE_NAME.includes(NameRol)){
+    const indexRol = ROLE_NAME.indexOf(NameRol)
+    console.log(ROLE_NAME[indexRol])
+    return indexRol + 1
+  }
+  return -1
+}
