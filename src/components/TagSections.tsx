135361a9dc1cf62dfcd6f83ae60096652da46dff
Julio Rodríguez <julio10489@gmail.com>
2024-10-12 16:02:01 -0500

    Agregado: Maquetación de la creación del tag

---
+++ src/components/TagSections.tsx
@@ -0,0 +1,47 @@
+import React, { HTMLAttributes, useEffect } from 'react'
+import { Input } from './ui/Input'
+import { Button } from './ui/Button'
+import { useProductStorage } from '@/zustand/ProductStorage'
+import { useURLStorage } from '@/zustand/URLStorage'
+import { IconPlus } from '@tabler/icons-react'
+import { RouteImage } from '@/lib/CreateRouteImage'
+
+interface Props extends HTMLAttributes<HTMLDivElement> {
+
+}
+export const CreateTag: React.FC<Props> = ({ ...props }) => {
+  const { AllProducts, getProducts } = useProductStorage()
+  const { Products } = useURLStorage()
+  useEffect(() => {
+    if (AllProducts.length == 0) {
+      const params = new URLSearchParams()
+      params.append("PageSize", "100")
+      params.append("PageNumber", "1")
+      getProducts(Products + params.toString())
+    }
+  }, [])
+  return (
+    <div {...props} className={`w-full flex flex-col justify-center items-center px-2 py-4 gap-2`}>
+      <h2 className='text-3xl font-bold font-oswald'>Crear nuevo tag</h2>
+      <Input />
+      <div className=''>
+        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6'>
+          {
+            AllProducts.map(product => <article className='w-40 max-h-72 border border-solid border-black flex flex-col justify-center ' key={product.id}>
+              <section className='w-full h-3/5'>
+                <img src={RouteImage(product.thumbnail)} className='w-full h-full' alt={`imagen de ${product.name}`} />
+              </section>
+              <main className='w-full h-full flex flex-col justify-center items-center px-2 py-1'>
+                <h6 className='text-pretty'> {product.name} </h6>
+                <span className='bg-green rounded-xl'>
+                  <IconPlus className='text-white font-bold' />
+                </span>
+              </main>
+            </article>)
+          }
+        </div>
+      </div>
+      <Button primary size='extraLarge'>Crear nuevo tag</Button>
+    </div>
+  )
+}
--- src/pages/SetTags.tsx
+++ src/pages/SetTags.tsx
@@ -1,15 +1,23 @@
 import { Layout } from '@/components/Layout'
-import { Button } from '@/components/ui/Button'
-import { Input } from '@/components/ui/Input'
-import React from 'react'
+import { TabsCollection } from '@/components/TabsCollection'
+import { CreateTag } from '@/components/TagSections'
+import { Tab } from '@/components/ui/Tab'
+import React, { useState } from 'react'
+
+type TShowTab = "CREATE" | "SET"
 
 export const SetTags: React.FC = () => {
+  const [showTab, setShowTab] = useState<TShowTab>("CREATE")
   return (
     <Layout>
       <main className='w-full h-full my-10 bg-bgLight px-2 py-4'>
-        <h1>Crear nueva etiqueta</h1>
-        <Input placeholder='3x3'/>
-        <Button size='extraLarge' primary={true}>Crear nueva etiqueta</Button>
+        <TabsCollection>
+          <Tab onClick={()=> setShowTab("CREATE")} TabTitle='Crear nuevo tag' state={showTab == "CREATE"} />
+          <Tab TabTitle='Asignar tag' state={showTab == "SET"} onClick={()=> setShowTab("SET")} />
+        </TabsCollection>
+        {
+          showTab == "CREATE" && <CreateTag />
+        }
       </main>
     </Layout>
   )
