55698ce6e2fadc7e94310a903a182818323858b2
Julio Rodríguez <julio10489@gmail.com>
2024-10-12 16:31:49 -0500

    Agregado: Pool de productos a los que se le agregaran la tag

--- src/components/TagSections.tsx
+++ src/components/TagSections.tsx
@@ -1,9 +1,9 @@
-import React, { HTMLAttributes, useEffect } from 'react'
+import React, { HTMLAttributes, useEffect, useState } from 'react'
 import { Input } from './ui/Input'
 import { Button } from './ui/Button'
 import { useProductStorage } from '@/zustand/ProductStorage'
 import { useURLStorage } from '@/zustand/URLStorage'
-import { IconPlus } from '@tabler/icons-react'
+import { IconMinus, IconPlus } from '@tabler/icons-react'
 import { RouteImage } from '@/lib/CreateRouteImage'
 
 interface Props extends HTMLAttributes<HTMLDivElement> {
@@ -12,6 +12,7 @@ interface Props extends HTMLAttributes<HTMLDivElement> {
 export const CreateTag: React.FC<Props> = ({ ...props }) => {
   const { AllProducts, getProducts } = useProductStorage()
   const { Products } = useURLStorage()
+  const [ProductsIds, setProductsIds] = useState<number[]>([])
   useEffect(() => {
     if (AllProducts.length == 0) {
       const params = new URLSearchParams()
@@ -20,22 +21,39 @@ export const CreateTag: React.FC<Props> = ({ ...props }) => {
       getProducts(Products + params.toString())
     }
   }, [])
+  const handleClickAdd = (IdProduct:number)=>{
+    if(ProductsIds.includes(IdProduct)) return
+    setProductsIds(prev => [...prev, IdProduct])
+  }
+  const handleClickDelete = (IdProduct:number)=>{
+    if(!ProductsIds.includes(IdProduct)) return
+    const aux = ProductsIds.filter(id => id != IdProduct)
+    setProductsIds(aux)
+  }
   return (
     <div {...props} className={`w-full flex flex-col justify-center items-center px-2 py-4 gap-2`}>
       <h2 className='text-3xl font-bold font-oswald'>Crear nuevo tag</h2>
-      <Input />
+      <Input placeholder='3x3' />
       <div className=''>
         <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6'>
           {
-            AllProducts.map(product => <article className='w-40 max-h-72 border border-solid border-black flex flex-col justify-center ' key={product.id}>
+            AllProducts.map(product => <article className='select-none w-40 max-h-72 border border-solid border-black flex flex-col justify-center ' key={product.id}>
               <section className='w-full h-3/5'>
                 <img src={RouteImage(product.thumbnail)} className='w-full h-full' alt={`imagen de ${product.name}`} />
               </section>
               <main className='w-full h-full flex flex-col justify-center items-center px-2 py-1'>
                 <h6 className='text-pretty'> {product.name} </h6>
-                <span className='bg-green rounded-xl'>
-                  <IconPlus className='text-white font-bold' />
-                </span>
+                {
+                  !ProductsIds.includes(product.id) ? (
+                    <span onClick={()=> handleClickAdd(product.id)} className='bg-green rounded-xl cursor-pointer select-none transition-all ease-in-out hover:opacity-65'>
+                      <IconPlus className='text-white font-bold' />
+                    </span>
+                  ) : (
+                    <span onClick={()=>handleClickDelete(product.id)} className='bg-red-700 rounded-xl cursor-pointer select-none transition-all ease-in-out hover:opacity-65'>
+                      <IconMinus className='text-white font-bold' />
+                    </span>
+                  )
+                }
               </main>
             </article>)
           }
