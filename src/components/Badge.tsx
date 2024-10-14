6f0043fd1153fa0abbd13ce24b9ec608917babce
Julio Rodríguez <julio10489@gmail.com>
2024-10-14 11:31:14 -0500

    Agregado: se le añadieron los atributos del elemento al componente badge

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, { HTMLAttributes } from 'react'
 
-interface Props {
+interface Props extends HTMLAttributes<HTMLSpanElement> {
   title: string
 }
-export const Badge: React.FC<Props> = ({title}) => {
+export const Badge: React.FC<Props> = ({ title, ...props }) => {
   return (
-    <span className=' text-sm bg-green text-white text-nowrap rounded p-1'> {title} </span>
+    <span {...props} className=' text-sm bg-green text-white text-nowrap rounded p-1'> {title} </span>
   )
 }
