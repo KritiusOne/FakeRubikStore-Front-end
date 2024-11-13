5fd0c6ec33bad4e4f216f8864f6e074f67bb36c4
Julio Rodríguez <julio10489@gmail.com>
2024-11-13 17:00:30 -0500

    Added: new info in to the product storage, min and max prices for search products

--- src/zustand/ProductStorage.ts
+++ src/zustand/ProductStorage.ts
@@ -4,12 +4,30 @@ import { create } from "zustand";
 interface ProductStorageTypes {
   AllProducts: Product[]
   BestProducts: Product[]
+  MinPriceValue: number
+  MaxPriceValue: number
   getProductsMock: (newProducts: ProductMock[])=> void
   getProducts: (url: string)=> void
+  setMinPrice: (newMinPrice: number)=>void
+  setMaxPrice: (newMaxPrice: number)=>void
 }
-export const useProductStorage = create<ProductStorageTypes>((set)=>({
+export const useProductStorage = create<ProductStorageTypes>((set, get)=>({
   AllProducts: [],
   BestProducts: [],
+  MaxPriceValue: 250000,
+  MinPriceValue: 0,
+  setMinPrice(newMinPrice) {
+    const Storage = get()
+    if(newMinPrice <= 100000){
+      set({...Storage, MinPriceValue: newMinPrice})
+    }
+  },
+  setMaxPrice(newMaxPrice) {
+    const Storage = get()
+    if(newMaxPrice >= 100000){
+      set({...Storage, MaxPriceValue: newMaxPrice})
+    }
+  },
   getProductsMock: (newProucts)=>{
     const formatersProducts = newProucts.map((product)=>getFormatProducts(product))
     const sortAllProducts = formatersProducts.sort((product)=> product.price)
