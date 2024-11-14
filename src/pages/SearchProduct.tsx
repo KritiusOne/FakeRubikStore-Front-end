ec4dcc8d30ddb4fbd0313dcf17d24d0f8beb23fd
Julio Rodríguez <julio10489@gmail.com>
2024-11-14 16:04:21 -0500

    Agregado: Implementado primer intento de conexión con el back-end

--- src/pages/SearchProduct.tsx
+++ src/pages/SearchProduct.tsx
@@ -3,19 +3,57 @@ import { Layout } from '@/components/Layout'
 import { CardProduct } from '@/components/ui/CardProduct'
 import { Spinner } from '@/components/ui/Spinner'
 import { AllDataProduct } from '@/types/ProductsTypes'
+import { PaginatedResponse } from '@/types/ResponseTypes'
+import { useProductStorage } from '@/zustand/ProductStorage'
 import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
 
 export const SearchProduct: React.FC = () => {
-  const [products, setProducts] = useState<AllDataProduct[]>([])
+  const { load, setLoad } = useProductStorage()
   const location = useLocation()
+  const [products, setProducts] = useState<PaginatedResponse<AllDataProduct[]>>({
+    metaData: {
+      currentPage: 0,
+      pageSize: 0,
+      totalCount: 0,
+      totalPage: 0,
+      hasNextPage: false,
+      hasPreviousPage: false,
+      previousPageURL: "",
+      nextPageURL: ""
+    },
+    msg: "",
+    statusCode: 0,
+    response: [],
+  })
   useEffect(()=>{
     const getSearchedProducts = async()=>{
-      const href = window.location.href
-      console.log({location, href})
+      const params = new URLSearchParams(location.search)
+      const URL = import.meta.env.VITE_API_URL_GET_ALL_PRODUCTS_ALL_INFO
+      const FINAL_URL = `${URL}?${params.toString()}`
+      setLoad(true)
+      try {        
+        const res = await fetch(FINAL_URL)
+        if(res.ok){
+          const response: PaginatedResponse<AllDataProduct[]> = await res.json()
+          if(products.metaData.currentPage != response.metaData.currentPage &&response.metaData.currentPage > 1){
+            setProducts({
+              ...response,
+              response: [...products.response, ...response.response]
+            })
+          }else{
+            setProducts({...response})
+          }
+          setProducts(response)
+        }
+      } catch (error) {
+        console.log(error)
+      }finally {
+        setLoad(false)
+      }
     }
     getSearchedProducts()
-  }, [])
+  }, [location.search])
   return (
     <Layout className='flex flex-col gap-2 justify-center items-center'>
       <div className='w-full h-full my-5 bg-bgLight flex flex-col justify-start items-center px-4 py-2'>
@@ -26,10 +64,10 @@ export const SearchProduct: React.FC = () => {
           </aside>
           <main className='flex flex-col flex-1 justify-center items-center px-4 py-2 gap-2'>
             {
-              products.length == 0 && <Spinner />
+              products.response.length == 0 && <Spinner />
             }
             {
-              products.length != 0 && products.map((product)=> <CardProduct price={product.price}  
+              products.response.length != 0 && !load && products.response.map((product)=> <CardProduct price={product.price}  
               productId={product.id}
               thumbnail={product.thumbnail} 
               title={product.name}
